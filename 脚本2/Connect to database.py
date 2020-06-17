51aad681936596c0ae49ac2918994179e65ef6ce
张磊 <zhang.lei@xyb2b.com>
2020-06-17 22:27:43 +0800

    修改对比逻辑

--- 脚本2/Connect to database.py
+++ 脚本2/Connect to database.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import decimal  # 用于十进制数学计算
 
 # 1、未使用优惠券： 佣金总额=下单数量 * 佣金费
 # 2、使用优惠券： 佣金总额= 实付总额*佣金率
@@ -11,42 +12,95 @@ mycursor = mydb.cursor()
 
 forder_id = input("请输入联盟订单号：")
 
-sql0 = "SELECT fsku_discount_amount FROM t_order_sku WHERE forder_id = '%s'" % (forder_id)  # 订单优惠总额
-sql1 = "SELECT fsku_num * fservice_charge FROM t_order_sku WHERE forder_id = '%s'" % (forder_id)
-sql2 = "SELECT fshould_server_total_amount FROM t_order_sku WHERE forder_id = '%s'" % (forder_id)
-sql3 = "SELECT fsku_pay_amount * fservice_charge / fsku_sale_price FROM t_order_sku WHERE forder_id = '%s'" % (
-	forder_id)
+forder_source = "SELECT forder_source FROM t_order WHERE fid = '%s'" % (forder_id)  # 订单来源 1api 2excel 3自营社交'
+mycursor.execute(forder_source)
+forder_source = mycursor.fetchall()  # fetchall() 获取所有数据
+for x in forder_source:
+	forder_source = x[0]
+	print("订单来源:", forder_source)
 
-mycursor.execute(sql0)
+fsku_discount_amount = "SELECT fsku_discount_amount FROM t_order_sku WHERE forder_id = '%s'" % (forder_id)  # 订单优惠总额
+mycursor.execute(fsku_discount_amount)
 fsku_discount_amount = mycursor.fetchall()
-print("订单优惠总额:",fsku_discount_amount)
+for x in fsku_discount_amount:
+	fsku_discount_amount = x[0]
+	print("优惠总额:", fsku_discount_amount)
 
-if fsku_discount_amount == [(0,)]:
+fsku_num = "SELECT fsku_num FROM t_order_sku WHERE forder_id = '%s'" % (forder_id)  # 订单sku数量
+mycursor.execute(fsku_num)
+fsku_num = mycursor.fetchall()
+for x in fsku_num:
+	fsku_num = x[0]
+	print("订单sku数量:", fsku_num)
+
+fservice_charge = "SELECT fservice_charge FROM t_order_sku WHERE forder_id = '%s'" % (forder_id)  # 订单佣金费
+mycursor.execute(fservice_charge)
+fservice_charge = mycursor.fetchall()
+for x in fservice_charge:
+	fservice_charge = x[0]
+	print("订单佣金费:", fservice_charge)
+
+fsku_pay_amount = "SELECT fsku_pay_amount FROM t_order_sku WHERE forder_id = '%s'" % (forder_id)  # 订单实付金额
+mycursor.execute(fsku_pay_amount)
+fsku_pay_amount = mycursor.fetchall()
+for x in fsku_pay_amount:
+	fsku_pay_amount = x[0]
+	print("订单实付总额:", fsku_pay_amount)
+
+fsku_sale_price = "SELECT fsku_sale_price FROM t_order_sku WHERE forder_id = '%s'" % (forder_id)  # 联盟销售单价
+mycursor.execute(fsku_sale_price)
+fsku_sale_price = mycursor.fetchall()
+for x in fsku_sale_price:
+	fsku_sale_price = x[0]
+	print("联盟销售单价:", fsku_sale_price)
+
+fshare_ratio = "SELECT fshare_ratio FROM t_order_sku WHERE forder_id = '%s'" % (forder_id)  # 推手服务费分成比例
+mycursor.execute(fshare_ratio)
+fshare_ratio = mycursor.fetchall()
+for x in fshare_ratio:
+	fshare_ratio = x[0] / 10000
+	print("推手服务费分成比例:", fshare_ratio)
+
+fservice_charge_total = "SELECT fservice_charge_total FROM t_order_sku WHERE forder_id = '%s'" % (forder_id)  # 导购端服务费总额
+mycursor.execute(fservice_charge_total)
+fservice_charge_total = mycursor.fetchall()
+for x in fservice_charge_total:
+	fservice_charge_total = x[0]
+	print("导购端服务费总额:", fservice_charge_total)
+
+fpush_hand_service_charge_total = "SELECT fpush_hand_service_charge_total FROM t_order_sku WHERE forder_id = '%s'" % (
+	forder_id)  # 推手服务费总额
+mycursor.execute(fpush_hand_service_charge_total)
+fpush_hand_service_charge_total = mycursor.fetchall()
+for x in fpush_hand_service_charge_total:
+	fpush_hand_service_charge_total = x[0]
+	print("推手服务费总额:", fpush_hand_service_charge_total)
+
+if forder_source == 3 and fsku_discount_amount == 0:
 	# 未使用优惠券
-	mycursor.execute(sql1)
-	serviceChargeTotal = mycursor.fetchall()  # fetchall() 获取所有数据
-	print("订单佣金费总额:", serviceChargeTotal)
+	fpush_hand_service_charge_total_my = fsku_num * fservice_charge * fshare_ratio
+	print("计算推手服务费总额:", fpush_hand_service_charge_total_my)
 
-	mycursor.execute(sql2)
-	fshould_server_total_amount = mycursor.fetchall()  # fetchall() 获取所有数据
-	print("应结算服务费总金额:", fshould_server_total_amount)
+	fservice_charge_total_my = fsku_num * fservice_charge * (1 - fshare_ratio)
+	print("计算导购端服务费总额:", fservice_charge_total_my)
 
-	if serviceChargeTotal == fshould_server_total_amount:
-		print("通过")
+	if fpush_hand_service_charge_total == fpush_hand_service_charge_total_my and fservice_charge_total == fservice_charge_total_my:
+		print("pass")
 	else:
-		print("不通过")
+		print("fail")
 
-else:
+elif forder_source == 3 and fsku_discount_amount != 0:
 	# 使用优惠券
-	mycursor.execute(sql3)
-	serviceChargeTotal = mycursor.fetchall()  # fetchall() 获取所有数据
-	print("订单佣金费总额:", serviceChargeTotal)
+	fpush_hand_service_charge_total_my = fsku_pay_amount * (fservice_charge / fsku_sale_price) * fshare_ratio
+	print("计算推手服务费总额:",
+		  decimal.Decimal(value=fpush_hand_service_charge_total_my).quantize(exp=decimal.Decimal(value='0')))
 
-	mycursor.execute(sql2)
-	fshould_server_total_amount = mycursor.fetchall()  # fetchall() 获取所有数据
-	print("应结算服务费总金额:", fshould_server_total_amount)
+	fservice_charge_total_my = fsku_pay_amount * (fservice_charge / fsku_sale_price) * (1 - fshare_ratio)
+	print("计算导购端服务费总额:", decimal.Decimal(value=fservice_charge_total_my).quantize(exp=decimal.Decimal(value='0')))
 
-	if serviceChargeTotal == fshould_server_total_amount:
-		print("通过")
+	if fpush_hand_service_charge_total == decimal.Decimal(value=fpush_hand_service_charge_total_my).quantize(
+			exp=decimal.Decimal(value='0')) and fservice_charge_total == decimal.Decimal(
+			value=fservice_charge_total_my).quantize(exp=decimal.Decimal(value='0')):
+		print("pass")
 	else:
-		print("不通过")
+		print("fail")
